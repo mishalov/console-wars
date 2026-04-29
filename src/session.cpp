12b904b22d9835f9389ed351784e5d09d6713f16
evgenii.mishalov@sap.com <evgenii.mishalov@sap.com>
2026-04-29 13:55:19 +0200

    Update

--- src/session.cpp
+++ src/session.cpp
@@ -5,11 +5,12 @@ void Session::queue_output(const std::string& data) {
 }
 
 void Session::send_telnet_init() {
-    // IAC WILL ECHO, IAC WILL SGA, IAC DONT LINEMODE
+    // Put client into character-at-a-time mode:
+    // WILL ECHO: server will echo (client should not local-echo)
+    // WILL SGA: suppress go-ahead (enables character mode)
     const char init[] = {
-        '\xff', '\xfb', '\x01',   // WILL ECHO
-        '\xff', '\xfb', '\x03',   // WILL SGA
-        '\xff', '\xfe', '\x22',   // DONT LINEMODE
+        '\xff', '\xfb', '\x01',   // IAC WILL ECHO
+        '\xff', '\xfb', '\x03',   // IAC WILL SGA
     };
     out_buf.append(init, sizeof(init));
 }
